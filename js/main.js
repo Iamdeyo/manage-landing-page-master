3282064fd2f4da328fa079723c95e732b01a2f43
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2022-10-10 08:08:40 +0100

    adding more slider features

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const slider = document.getElementById('slider');
+const carouselBtn = document.querySelectorAll('.carousel-btn');
+const sliderItem = document.querySelector('#slider > div');
+let carouselScroll = sliderItem.clientWidth;
+console.log(slider.scrollWidth);
+// console.log(slider.offsetWidth);
+// slider.addEventListener('click', () => {
+//   //   console.log(slider1.getBoundingClientRect().left);
+//   console.log(slider.scrollLeft);
+//   //   console.log(slider.childElementCount);
+// });
+
+// test.addEventListener('click', () => {
+//   slider.scrollTo(500, 00);
+//   console.log(slider.scrollLeft);
+
+//   console.log('clicked');
+// });
+
+const scrollCarousel = () => {
+  carouselBtn.forEach((cr, i) => {
+    console.log(i);
+    // const removeClass = () => {
+    //   cr.classList.remove('bg-brightRed');
+    //   cr.classList.add('bg-brightRed');
+
+    //   console.log(1);
+    // };
+
+    slider.addEventListener('click', () => {
+      console.log(slider.scrollLeft);
+      console.log(carouselScroll * i + 16 + '-----');
+      if (slider.scrollLeft === carouselScroll * i) {
+        console.log(cr);
+      }
+    });
+
+    cr.addEventListener('click', () => {
+      slider.scrollTo(carouselScroll * i, 00);
+    });
+  });
+};
+scrollCarousel();
